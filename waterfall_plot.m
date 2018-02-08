703d46c38f8dcc3ad9924618fd9dd2accad1cf20
ikmckenz <sirreal.ian@gmail.com>
2018-02-07 23:51:27 -0700

    matlab script mostly working

--- waterfall_plot.m
+++ waterfall_plot.m
@@ -1,11 +1,24 @@
 clear; clc;
 
 Z = double(cell2mat(struct2cell(load('z_values.mat'))));
-X = double(cell2mat(struct2cell(load('x_values.mat'))));
-Y = double(cell2mat(struct2cell(load('y_values.mat'))));
+Y = string(cell2mat(struct2cell(load('x_values.mat'))));
 
-X = 1:size(Z);
+% Flip both so the most recent data is at the top
+Z = flipud(Z);
+Y = flipud(Y);
 
-colormap copper;
-waterfall(Z);
+% Animate the plot panning into shape
+for i=2275:5:2275
+    h = waterfall(Z);
+    set(h, 'EdgeColor', 'k');
+    ticks = 1:1000:size(Y);
+    yticks(ticks);
+    yticklabels(Y(ticks));
+    xticks([1,6,12,18,24]);
+    set(gca, 'YLim', get(gca, 'YLim')+i);
+    filename = sprintf('shots/img%04d.png', length(Y)-i);
+    saveas(h, filename);
+    disp(i)
+end
+
 
